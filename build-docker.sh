795c68a9e995b977ff302b257eb88d1125fcb613
Oliver Freyermuth <o.freyermuth@googlemail.com>
2015-12-29 16:58:03 +0100

    don't detach now anymore!

--- build-docker.sh
+++ build-docker.sh
@@ -3,4 +3,4 @@
 docker create -v /usr/portage --name portage gentoo/portage
 docker cp build-script.sh gentoo:/
 
-docker run --volumes-from portage --name gentoo -it -d gentoo/stage3-amd64 /build-script.sh
+docker run --volumes-from portage --name gentoo -it gentoo/stage3-amd64 /build-script.sh
