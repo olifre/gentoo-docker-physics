7a854d90e56493633b0da9f4abc1d5404e49fd23
Oliver Freyermuth <o.freyermuth@googlemail.com>
2015-12-29 16:47:16 +0100

    detach running container

--- build-docker.sh
+++ build-docker.sh
@@ -2,7 +2,7 @@
 
 docker create -v /usr/portage --name portage gentoo/portage
 
-docker run --volumes-from portage --name gentoo -it gentoo/stage3-amd64 /bin/bash
+docker run --volumes-from portage --name gentoo -it -d gentoo/stage3-amd64 /bin/bash
 docker exec gentoo sh -c "echo \"GENTOO_MIRRORS=\"http://distfiles.gentoo.org/\"\" >> /etc/portage/make.conf"
 docker exec gentoo mkdir -p /usr/portage
 docker exec gentoo chown -R portage:portage /usr/portage
