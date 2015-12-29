7f5625bba1e19d76765b96dc0a04bd887b02dfc1
Oliver Freyermuth <o.freyermuth@googlemail.com>
2015-12-29 16:45:54 +0100

    install ccache and set it up.

--- build-docker.sh
+++ build-docker.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
 docker create -v /usr/portage --name portage gentoo/portage
+
 docker run --volumes-from portage --name gentoo -it gentoo/stage3-amd64 /bin/bash
 docker exec gentoo sh -c "echo \"GENTOO_MIRRORS=\"http://distfiles.gentoo.org/\"\" >> /etc/portage/make.conf"
 docker exec gentoo mkdir -p /usr/portage
 docker exec gentoo chown -R portage:portage /usr/portage
 docker exec gentoo emerge-webrsync
 docker exec gentoo eselect news read new
-docker exec gentoo emerge -v gentoolkit
+docker exec gentoo emerge -v gentoolkit ccache
+docker exec gentoo sh -c "sed -i 's/FEATURES=\"/FEATURES=\"ccache /' /etc/portage/make.conf"
+docker exec gentoo sh -c "echo CCACHE_SIZE=\"2G\" >> /etc/portage/make.conf"
 docker exec gentoo sh -c 'rm -rf /usr/portage/*'
