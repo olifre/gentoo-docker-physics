bff41d9c28d4a3c8e6948e8e84e34ce71917922c
Oliver Freyermuth <o.freyermuth@googlemail.com>
2015-12-29 23:41:17 +0100

    root-prepare: Do depclean after clang compile for consistency (remove build-time deps)

--- build-script.sh
+++ build-script.sh
@@ -7,12 +7,12 @@ cp /build/root-dbg /etc/portage/package.env/
 echo "sci-physics/root" >> /etc/portage/package.keywords
 euse -E graphviz http postgres sqlite qt4 math xml clang qt3support jpeg gif png tiff truetype fontconfig
 
+# Compile some heavy stuff which is needed for ROOT
+emerge -j3 -v clang
+
 # Update once more
 emerge -j3 -uDNv @system @world
 emerge --depclean
-
-# Compile some heavy stuff which is needed for ROOT
-emerge -j3 -v clang
 
 # CCACHE stats
 CCACHE_DIR="/var/tmp/ccache" ccache -s
