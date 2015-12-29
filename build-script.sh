c9d8734bda63122608b1af300a68dc175789be68
Oliver Freyermuth <o.freyermuth@googlemail.com>
2015-12-29 16:54:33 +0100

    fixup cleanup

--- build-script.sh
+++ build-script.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 
 echo 'GENTOO_MIRRORS="http://distfiles.gentoo.org/"' >> /etc/portage/make.conf
-mkdir -p /usr/portage
-chown -R portage:portage /usr/portage
-emerge-webrsync
+#mkdir -p /usr/portage
+#chown -R portage:portage /usr/portage
+#emerge-webrsync
 eselect news read new
 emerge -v gentoolkit ccache
 sed -i 's/FEATURES="/FEATURES="ccache /' /etc/portage/make.conf
 echo 'CCACHE_SIZE="2G"' >> /etc/portage/make.conf
-rm -rf /usr/portage/*'
+
+# Cleanup
+#rm -rf /usr/portage/*'
+
+CCACHE_DIR="/var/tmp/ccache" ccache -s
+
+# Self-destruct
 rm -v -rf $0
