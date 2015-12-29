785ad537795c7025160d9587a1040dddddf6f0a3
Oliver Freyermuth <o.freyermuth@googlemail.com>
2015-12-29 23:58:30 +0100

    depclean twice, better - and compile more heavy stuff to relieve the ROOT-build.

--- build-script.sh
+++ build-script.sh
@@ -7,8 +7,12 @@ cp /build/root-dbg /etc/portage/package.env/
 echo "sci-physics/root" >> /etc/portage/package.keywords
 euse -E graphviz http postgres sqlite qt4 math xml clang qt3support jpeg gif png tiff truetype fontconfig
 
+# Update after flag changes
+emerge -j3 -uDNv @system @world
+emerge --depclean
+
 # Compile some heavy stuff which is needed for ROOT
-emerge -j3 -v clang
+emerge -j3 -v sys-devel/clang virtual/opengl dev-util/cmake media-gfx/graphviz
 
 # Update once more
 emerge -j3 -uDNv @system @world
