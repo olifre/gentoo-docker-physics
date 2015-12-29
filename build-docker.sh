489daf44c1912eb673063d0837d431a01f13019a
Oliver Freyermuth <o.freyermuth@googlemail.com>
2015-12-29 16:38:09 +0100

    try to fix quoting

--- build-docker.sh
+++ build-docker.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-CONTAINER_ID=$(docker create -t -i -d gentoo/stage3-amd64 bash)
-docker start -a -i $CONTAINER_ID
-docker exec $CONTAINER_ID sh -c 'echo \'GENTOO_MIRRORS="http://distfiles.gentoo.org/"\' >> /etc/portage/make.conf'
-docker exec $CONTAINER_ID mkdir -p /usr/portage
-docker exec $CONTAINER_ID chown -R portage:portage /usr/portage
-docker exec $CONTAINER_ID emerge-webrsync
-docker exec $CONTAINER_ID eselect news read new
-docker exec $CONTAINER_ID emerge -v gentoolkit
-docker exec $CONTAINER_ID sh -c 'rm -rf /usr/portage/*'
+docker create -v /usr/portage --name portage gentoo/portage
+docker run --volumes-from portage --name gentoo -it gentoo/stage3-amd64 /bin/bash
+docker exec gentoo sh -c "echo \"GENTOO_MIRRORS=\"http://distfiles.gentoo.org/\"\" >> /etc/portage/make.conf"
+docker exec gentoo mkdir -p /usr/portage
+docker exec gentoo chown -R portage:portage /usr/portage
+docker exec gentoo emerge-webrsync
+docker exec gentoo eselect news read new
+docker exec gentoo emerge -v gentoolkit
+docker exec gentoo sh -c 'rm -rf /usr/portage/*'
