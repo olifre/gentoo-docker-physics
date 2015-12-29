429b8e9f67c7816b7a1af6616775f4e833b82d92
Oliver Freyermuth <o.freyermuth@googlemail.com>
2015-12-29 18:16:44 +0100

    modularize build-script

--- build-docker.sh
+++ build-docker.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -x
 
+INPUT_CONTAINER=${1:-gentoo/stage3-amd64}
+OUTPUT_CONTAINER=${2:-olifre/gentoo-docker-physics.FIXME}
+
 docker create -v /usr/portage --name portage gentoo/portage
 
 mkdir -p ~/.ccache/
@@ -7,7 +10,7 @@ docker run --volumes-from portage \
            -v ~/.ccache:/var/tmp/ccache \
            -v $(pwd):/build \
            --name gentoo \
-           gentoo/stage3-amd64 \
+           ${INPUT_CONTAINER} \
            /build/build-script.sh
 
-docker commit gentoo olifre/gentoo-docker-physics.setup
+docker commit gentoo ${OUTPUT_CONTAINER}
