f19159b550d556633956cb44ae3a66a5ea42d332
Oliver Freyermuth <o.freyermuth@googlemail.com>
2015-12-30 23:37:05 +0100

    modularize a bit, use binpkg

--- build-docker.sh
+++ build-docker.sh
@@ -4,11 +4,14 @@ INPUT_CONTAINER=${1:-gentoo/stage3-amd64}
 OUTPUT_CONTAINER=${2:-olifre/gentoo-docker-physics.FIXME}
 BRANCHNAME=${3:-master}
 
+mkdir -p ~/.ccache-${BRANCHNAME}/
+mkdir -p ~/packages-${BRANCHNAME}/
+
 docker create -v /usr/portage --name portage gentoo/portage
 
-mkdir -p ~/.ccache-${BRANCHNAME}/
 docker run --volumes-from portage \
            -v ~/.ccache-${BRANCHNAME}:/var/tmp/ccache \
+           -v ~/packages-${BRANCHNAME}:/var/packages \
            -v $(pwd):/build \
            --name gentoo \
            ${INPUT_CONTAINER} \
--- build-script.sh
+++ build-script.sh
@@ -1,14 +1,8 @@
 #!/bin/bash -ex
 
-# CCACHE stats
-CCACHE_DIR="/var/tmp/ccache" ccache -s
-
 # Compile ROOT
 emerge -v -j3 root
 
 # Update a bit and cleanup
 emerge -j3 -uDNv @system @world
 emerge --depclean
-
-# CCACHE stats
-CCACHE_DIR="/var/tmp/ccache" ccache -s
