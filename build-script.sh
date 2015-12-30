692e9dea5864538e5c89138342697699ce48ebd5
Oliver Freyermuth <o.freyermuth@googlemail.com>
2015-12-30 21:29:27 +0100

    remove duplicate ccache output

--- build-script.sh
+++ build-script.sh
@@ -1,8 +1,5 @@
 #!/bin/bash -ex
 
-# CCACHE stats
-CCACHE_DIR="/var/tmp/ccache" ccache -s
-
 mkdir -p /etc/portage/env/
 cp /build/debug.conf /etc/portage/env/
 mkdir -p /etc/portage/package.env/
@@ -15,6 +12,3 @@ emerge -j3 -v dev-util/debugedit sys-libs/glibc
 
 emerge -j3 -uDNv @system @world
 emerge --depclean
-
-# CCACHE stats
-CCACHE_DIR="/var/tmp/ccache" ccache -s
