58483dfb2319409f7c6a33ded2b0d5a43afb20e9
olifre <o.freyermuth@googlemail.com>
2015-12-31 05:37:17 +0100

    Play safe, package.use for example is a directory here

--- build-script.sh
+++ build-script.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -ex
 
 cp /build/root-dbg /etc/portage/package.env/
-echo "sci-physics/root" >> /etc/portage/package.keywords
+mkdir -p /etc/portage/package.keywords/
+echo "sci-physics/root" >> /etc/portage/package.keywords/root
 euse -E graphviz http postgres sqlite qt4 math xml clang qt3support jpeg gif png tiff truetype fontconfig
 
 # Update after flag changes
@@ -10,7 +11,8 @@ emerge --depclean
 
 # Compile some heavy stuff which is needed for ROOT
 # LLVM without python since that pulls in python2 again
-echo "sys-devel/llvm -python" >> /etc/portage/package.use
+mkdir -p /etc/portage/package.use/
+echo "sys-devel/llvm -python" >> /etc/portage/package.use/no-python2
 emerge -j3 -v sys-devel/clang virtual/opengl dev-util/cmake media-gfx/graphviz dev-db/postgresql
 
 # Update once more
