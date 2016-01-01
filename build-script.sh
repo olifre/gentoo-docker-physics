bc0b965a52c4d5c44aaf05abbf78de9c8d041ed3
Oliver Freyermuth <o.freyermuth@googlemail.com>
2016-01-01 20:58:30 +0100

    bleed more

--- build-script.sh
+++ build-script.sh
@@ -2,9 +2,9 @@
 
 # Keywords and flags for geant4
 mkdir -p /etc/portage/package.keywords
-echo "sci-physics/vgm" >> /etc/portage/package.keywords/physics-addons
-echo "sci-physics/geant-vmc:4" >> /etc/portage/package.keywords/physics-addons
-echo "sci-physics/genfit" >> /etc/portage/package.keywords/physics-addons
+echo "sci-physics/vgm **" >> /etc/portage/package.keywords/physics-addons
+echo "sci-physics/geant-vmc:4 **" >> /etc/portage/package.keywords/physics-addons
+echo "sci-physics/genfit **" >> /etc/portage/package.keywords/physics-addons
 mkdir -p /etc/portage/package.use
 echo "sci-physics/vgm geant4 root xml" >> /etc/portage/package.use/physics-addons
 echo "sci-physics/geant-vmc:4 vgm" >> /etc/portage/package.use/physics-addons
