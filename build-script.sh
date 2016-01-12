5fc88eebb864e1edc54ff2026b215eb478aed4bb
Oliver Freyermuth <o.freyermuth@googlemail.com>
2016-01-12 20:16:43 +0100

    safety [ci skip]

--- build-script.sh
+++ build-script.sh
@@ -1,5 +1,10 @@
 #!/bin/bash -ex
 
+# Safety.
+chown -R portage:portage /usr/portage
+chown -R portage:portage /var/tmp/ccache
+chown -R portage:portage /var/packages
+
 # Debug symbols
 mkdir -p /etc/portage/package.env/
 cp /build/geant-dbg /etc/portage/package.env/
